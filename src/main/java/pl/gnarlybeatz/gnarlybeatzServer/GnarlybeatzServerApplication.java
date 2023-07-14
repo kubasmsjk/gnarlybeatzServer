0519fa0ff3ae4bc28741ae59c97f0bb4afc089e4
kuba <j.mieczkowski00@gmail.com>
2023-07-14 16:59:05 +0200

    Initial commit

---
+++ src/main/java/pl/gnarlybeatz/gnarlybeatzServer/GnarlybeatzServerApplication.java
@@ -0,0 +1,28 @@
+package pl.gnarlybeatz.gnarlybeatzServer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class GnarlybeatzServerApplication {
+	private static final Logger logger = LoggerFactory.getLogger(GnarlybeatzServerApplication.class.getName());
+
+	public static void main(String[] args) throws UnknownHostException {
+		ConfigurableApplicationContext context = SpringApplication.run(GnarlybeatzServerApplication.class, args);
+		Environment env = context.getEnvironment();
+
+		logger.info("\n----------------------------------------------------------\n\t"
+						+"Application '{}' is running! Access URLs:\n\t"
+						+"Local: \t\thttp://localhost:{}\n\t"+"External: \thttp://{}:{}\n\t"
+						+"Profile(s): \t{}\n----------------------------------------------------------",env.getProperty("spring.application.name"),
+				env.getProperty("server.port"), InetAddress.getLocalHost().getHostAddress(),env.getProperty("server.port"),env.getActiveProfiles());
+	}
+
+}
---
+++ src/test/java/pl/gnarlybeatz/gnarlybeatzServer/GnarlybeatzServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gnarlybeatz.gnarlybeatzServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GnarlybeatzServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
