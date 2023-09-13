98ffe5e0d226dda93f1b8ea1433033e829d7f3b4
kuba <j.mieczkowski00@gmail.com>
2023-09-13 12:45:57 +0200

    adding authentication and register

---
+++ src/main/java/pl/gnarlybeatz/gnarlybeatzServer/auth/AuthenticationService.java
@@ -0,0 +1,48 @@
+package pl.gnarlybeatz.gnarlybeatzServer.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import pl.gnarlybeatz.gnarlybeatzServer.config.JwtService;
+import pl.gnarlybeatz.gnarlybeatzServer.user.Role;
+import pl.gnarlybeatz.gnarlybeatzServer.user.User;
+import pl.gnarlybeatz.gnarlybeatzServer.user.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = new User();
+        user.setFirstname(request.getFirstname());
+        user.setLastname(request.getLastname());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(Role.USER);
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+
+        return new AuthenticationResponse(jwtToken);
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("Could not find user"));
+        var jwtToken = jwtService.generateToken(user);
+        return new AuthenticationResponse(jwtToken);
+    }
+}
