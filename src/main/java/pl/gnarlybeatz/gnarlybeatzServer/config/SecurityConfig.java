3ec7429ea1756af737141940c1685e5c8f06ea78
kuba <j.mieczkowski00@gmail.com>
2023-09-29 12:56:37 +0200

    adding logout

--- src/main/java/pl/gnarlybeatz/gnarlybeatzServer/config/SecurityConfig.java
+++ src/main/java/pl/gnarlybeatz/gnarlybeatzServer/config/SecurityConfig.java
@@ -5,20 +5,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+    private final LogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -27,9 +32,9 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(
-                                "/**",
-                                "/v3/api-docs/**",
-                                "/api/v1/auth/**",
+                                "/v3/api-docs/**", //todo raczej potem usunac
+                                "/api/auth/**",
+                                "/sendMail",
                                 "/swagger-ui/**").permitAll()
                         .requestMatchers(HttpMethod.DELETE, "/api/main-post/**").hasRole("ADMIN")
                         .requestMatchers("/api/**").authenticated()
@@ -37,8 +42,12 @@ public class SecurityConfig {
                 )
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout(logout ->
+                        logout.logoutUrl("/api/v1/auth/logout")
+                                .addLogoutHandler(logoutHandler)
+                                .logoutSuccessHandler((request, response, authentication) ->
+                                        SecurityContextHolder.clearContext()));
         return http.build();
     }
 }
