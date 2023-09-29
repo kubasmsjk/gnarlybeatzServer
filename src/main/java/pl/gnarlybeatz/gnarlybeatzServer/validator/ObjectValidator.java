2faf57c9e5d0bf85cecd30afe7337234284957cf
kuba <j.mieczkowski00@gmail.com>
2023-09-29 12:57:36 +0200

    adding validator

---
+++ src/main/java/pl/gnarlybeatz/gnarlybeatzServer/validator/ObjectValidator.java
@@ -0,0 +1,26 @@
+package pl.gnarlybeatz.gnarlybeatzServer.validator;
+
+import jakarta.validation.*;
+import org.springframework.stereotype.Component;
+import pl.gnarlybeatz.gnarlybeatzServer.exceptions.ObjectNotValidException;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Component
+public class ObjectValidator<T> {
+
+    private final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = factory.getValidator();
+
+    public void validate(T objectToValidate) {
+        Set<ConstraintViolation<T>> violations = validator.validate(objectToValidate);
+
+        if (!violations.isEmpty()) {
+            var errorMessages = violations
+                    .stream()
+                    .collect(Collectors.toMap(ConstraintViolation::getPropertyPath, ConstraintViolation::getMessage));
+            throw new ObjectNotValidException(errorMessages);
+        }
+    }
+}
