483e5ac02ec16aa9be22f60d648f0837c7885e0e
kuba <j.mieczkowski00@gmail.com>
2023-09-29 12:53:48 +0200

    adding token functionality and register

--- src/main/java/pl/gnarlybeatz/gnarlybeatzServer/auth/AuthenticationController.java
+++ src/main/java/pl/gnarlybeatz/gnarlybeatzServer/auth/AuthenticationController.java
@@ -1,11 +1,15 @@
 package pl.gnarlybeatz.gnarlybeatzServer.auth;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -17,14 +21,25 @@ public class AuthenticationController {
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
-    ){
-        return ResponseEntity.ok(service.register(request));
+    ) {
+        final AuthenticationResponse authenticationResponse = service.register(request);
+        return ResponseEntity
+                .accepted()
+                .body(authenticationResponse);
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
-    ){
+    ) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        service.refreshToken(request, response);
     }
 }
--- src/main/java/pl/gnarlybeatz/gnarlybeatzServer/auth/AuthenticationService.java
+++ src/main/java/pl/gnarlybeatz/gnarlybeatzServer/auth/AuthenticationService.java
@@ -1,36 +1,69 @@
 package pl.gnarlybeatz.gnarlybeatzServer.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.gnarlybeatz.gnarlybeatzServer.config.JwtService;
+import pl.gnarlybeatz.gnarlybeatzServer.exceptions.ObjectNotValidException;
+import pl.gnarlybeatz.gnarlybeatzServer.exceptions.UserExistException;
+import pl.gnarlybeatz.gnarlybeatzServer.token.Token;
+import pl.gnarlybeatz.gnarlybeatzServer.token.TokenRepository;
+import pl.gnarlybeatz.gnarlybeatzServer.token.TokenType;
 import pl.gnarlybeatz.gnarlybeatzServer.user.Role;
 import pl.gnarlybeatz.gnarlybeatzServer.user.User;
 import pl.gnarlybeatz.gnarlybeatzServer.user.UserRepository;
+import pl.gnarlybeatz.gnarlybeatzServer.validator.ObjectValidator;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-    private final UserRepository repository;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+    private final ObjectValidator<RegisterRequest> userValidator;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        var user = new User();
-        user.setFirstname(request.getFirstname());
-        user.setLastname(request.getLastname());
-        user.setEmail(request.getEmail());
-        user.setPassword(passwordEncoder.encode(request.getPassword()));
-        user.setRole(Role.USER);
-        repository.save(user);
-        var jwtToken = jwtService.generateToken(user);
 
-        return new AuthenticationResponse(jwtToken);
+        checkIsUserExist(request.getEmail());
+        userValidator.validate(request);
+
+        var user = User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        var savedUser = userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
+        saveUserToken(savedUser, jwtToken);
+
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    private void checkIsUserExist(String email) {
+        var exist = userRepository.findByEmail(email);
+        if (exist.isPresent()) {
+            throw new UserExistException(Map.of("userExistStatus", true));
+        }
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
@@ -40,9 +73,64 @@ public class AuthenticationService {
                         request.getPassword()
                 )
         );
-        var user = repository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new UsernameNotFoundException("Could not find user"));
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
         var jwtToken = jwtService.generateToken(user);
-        return new AuthenticationResponse(jwtToken);
+        var refreshToken = jwtService.generateRefreshToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user, jwtToken);
+        return AuthenticationResponse.builder()
+                .accessToken(jwtToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoke(false)
+                .build();
+        tokenRepository.save(token);
+    }
+
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokensByUser(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(t -> {
+            t.setExpired(true);
+            t.setRevoke(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String refreshToken;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        refreshToken = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(refreshToken);
+        if (userEmail != null) {
+            var user = this.userRepository.findByEmail(userEmail).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            if (jwtService.isTokenValid(refreshToken, user)) {
+                var accessToken = jwtService.generateToken(user);
+                revokeAllUserTokens(user);
+                saveUserToken(user, accessToken);
+                var authResponse = AuthenticationResponse.builder()
+                        .accessToken(accessToken)
+                        .refreshToken(refreshToken)
+                        .build();
+                new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+            }
+        }
     }
 }
