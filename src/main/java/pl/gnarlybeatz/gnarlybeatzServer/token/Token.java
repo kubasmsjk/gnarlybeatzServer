53d8380c2b96e59fc9f432d2d5b382e4aa9a6516
kuba <j.mieczkowski00@gmail.com>
2024-01-23 14:28:32 +0100

    adding colum constraints

--- src/main/java/pl/gnarlybeatz/gnarlybeatzServer/token/Token.java
+++ src/main/java/pl/gnarlybeatz/gnarlybeatzServer/token/Token.java
@@ -17,9 +17,10 @@ public class Token {
     @Id
     @GeneratedValue
     private Long id;
-    @Column(unique = true)
+    @Column(unique = true, updatable = false)
     private String token;
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false, updatable = false)
     private TokenType tokenType = TokenType.BEARER;
     private boolean revoked;
     private boolean expired;
