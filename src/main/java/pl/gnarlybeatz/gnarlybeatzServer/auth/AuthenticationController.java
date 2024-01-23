ef751d82b2708e86e2c1416451cf151839db6135
kuba <j.mieczkowski00@gmail.com>
2024-01-23 14:22:39 +0100

    add cross origin for test

--- src/main/java/pl/gnarlybeatz/gnarlybeatzServer/auth/AuthenticationController.java
+++ src/main/java/pl/gnarlybeatz/gnarlybeatzServer/auth/AuthenticationController.java
@@ -4,10 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -18,6 +15,7 @@ public class AuthenticationController {
 
     private final AuthenticationService service;
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
@@ -25,6 +23,7 @@ public class AuthenticationController {
         return ResponseEntity.ok(service.register(request));
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
