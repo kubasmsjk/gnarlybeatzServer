921f51da14eb7426a671df0e0449d4feb35da742
kuba <j.mieczkowski00@gmail.com>
2024-02-09 18:00:57 +0100

    adding query findUserEmailByUserId

--- src/main/java/pl/gnarlybeatz/gnarlybeatzServer/user/UserRepository.java
+++ src/main/java/pl/gnarlybeatz/gnarlybeatzServer/user/UserRepository.java
@@ -1,10 +1,16 @@
 package pl.gnarlybeatz.gnarlybeatzServer.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    @Query("SELECT e.email FROM User e WHERE e.id = :id")
+    String findUserEmailByUserId(@Param("id") String id);
+
 }
