717e693740dee3cfe5311e825f2f6495eae493a0
kuba <j.mieczkowski00@gmail.com>
2024-02-09 17:50:56 +0100

    remove classes

--- src/main/java/pl/gnarlybeatz/gnarlybeatzServer/emial/EmailSender.java
+++
@@ -1,5 +0,0 @@
-package pl.gnarlybeatz.gnarlybeatzServer.emial;
-
-public interface EmailSender {
-    String send(EmailDetails emailDetails);
-}
--- src/main/java/pl/gnarlybeatz/gnarlybeatzServer/user/EditUser.java
+++
@@ -1,4 +0,0 @@
-package pl.gnarlybeatz.gnarlybeatzServer.user;
-
-public class EditUser {
-}
