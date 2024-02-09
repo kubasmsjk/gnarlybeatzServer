30cfe6b36a1a31b939bb522d5af63139cd37bdc5
kuba <j.mieczkowski00@gmail.com>
2024-02-09 18:02:31 +0100

    addition of role handling

--- src/main/java/pl/gnarlybeatz/gnarlybeatzServer/config/SecurityConfig.java
+++ src/main/java/pl/gnarlybeatz/gnarlybeatzServer/config/SecurityConfig.java
@@ -15,36 +15,54 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
+import static pl.gnarlybeatz.gnarlybeatzServer.user.Role.ADMIN;
+import static pl.gnarlybeatz.gnarlybeatzServer.user.Role.USER;
+
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
+    private final CorsConfig corsConfig;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request -> request
-                        .requestMatchers(
-                                "/v3/api-docs/**", //todo raczej potem usunac
-                                "/api/auth/**",
-                                "/sendMail",
-                                "/swagger-ui/**").permitAll()
-                        .requestMatchers(HttpMethod.DELETE, "/api/main-post/**").hasRole("ADMIN")
-                        .requestMatchers("/api/**").authenticated()
-                        .anyRequest().authenticated()
+                .cors((c) -> c.configurationSource(corsConfig.configurationCorsSource()))
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers(
+                                        "/v3/api-docs/**", //todo raczej potem usunac
+                                        "/swagger-ui/**",
+                                        "/api/email/**",
+                                        "/api/audio/search",
+                                        "/api/audio/filter/values",
+                                        "/api/auth/register",
+                                        "/api/auth/authenticate",
+                                        "/api/payments/stripe/events"
+                                )
+                                .permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/auth/refreshToken").hasAnyRole(ADMIN.name(), USER.name())
+                                .requestMatchers(HttpMethod.POST, "/api/user/edit").hasAnyRole(ADMIN.name(), USER.name())
+                                .requestMatchers(HttpMethod.POST, "/api/audio/archiveBeat").hasRole(ADMIN.name())
+                                .requestMatchers(HttpMethod.POST, "/api/audio/uploadOrUpdate").hasRole(ADMIN.name())
+                                .requestMatchers(HttpMethod.GET, "/api/audio/listOfAudioFilesToUpdate").hasRole(ADMIN.name())
+                                .requestMatchers(HttpMethod.GET, "/api/audio/listOfArchiveBeat").hasRole(ADMIN.name())
+                                .requestMatchers(HttpMethod.POST, "/api/audio/favoriteBeats/add").hasAnyRole(ADMIN.name(), USER.name())
+                                .requestMatchers(HttpMethod.POST, "/api/audio/favoriteBeats/remove").hasAnyRole(ADMIN.name(), USER.name())
+                                .requestMatchers(HttpMethod.GET, "/api/audio/download").hasRole(USER.name())
+                                .requestMatchers(HttpMethod.POST, "/api/payments/checkout").hasRole(USER.name())
+                                .anyRequest().authenticated()
                 )
-                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .logout(logout ->
-                        logout.logoutUrl("/api/v1/auth/logout")
+                        logout.logoutUrl("/api/auth/logout")
                                 .addLogoutHandler(logoutHandler)
                                 .logoutSuccessHandler((request, response, authentication) ->
                                         SecurityContextHolder.clearContext()));
